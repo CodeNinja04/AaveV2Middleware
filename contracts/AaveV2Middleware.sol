dc0c23a40c8566d5c0cf14e9f216532334962193
CodeNinja04 <hemendrasharma2019@gmail.com>
2022-04-25 22:48:18 +0530

    final updates✔️

--- contracts/AaveV2Middleware.sol
+++ contracts/AaveV2Middleware.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.1;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {ILendingPool} from "./ILendingPool.sol";
-import {ILendingPoolAddressesProvider} from "./ILendingPoolAddressesProvider.sol";
+//import {ILendingPool} from "./ILendingPool.sol";
+//import {ILendingPoolAddressesProvider} from "./ILendingPoolAddressesProvider.sol";
 
 import "./interfaces.sol";
 
